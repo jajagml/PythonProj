e3e07c8b3b19c9383e1c9f415575c859845ceac2
Jaja M. G <77393559+jajagml@users.noreply.github.com>
2023-11-21 22:14:14 +0800

    Update Polymorphism.py
    corrected issue found

--- PythonProj/Polymorphism.py
+++ PythonProj/Polymorphism.py
@@ -1,32 +1,32 @@
 # polymorphism - means many form
 # it refers to methods/functions/operators with the same name that can be executed on many objects or classes
 class MyElements:
-  def msgLog():
+  def msgLog(self):
     print("Element msg log")
 
-class Button(MyElements):
+class myButton(MyElements):
   def click():
     print("Button clicked.") # just representation
-  
-  def msgLog():
+
+  def msgLog(self):
     print("Button msg log")
 
-class Link(MyElements):
-  def linkText():
+class MyLink(MyElements):
+  def linkText(self):
     print("This is link text") # just representation
-    
-  def msgLog():
+
+  def msgLog(self):
     print("Link msg log")  
 
 class MyTextbox(MyElements):
   pass
 
 myBtn = myButton()
-myLink = MyLink()
-myTextBox = MyTextbox()
+myLink1 = MyLink()
+myTB = MyTextbox()
 
-for log in (myBtn, myLink, myTextBox):
-  print(log.msgLog())
+for log in (myBtn, myLink1, myTB):
+  log.msgLog()
 
 # msgLog() is the one that represents the polymorphism
 # msgLog() is present in each of the child class but it has different implementation
