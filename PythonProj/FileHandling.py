522a6d4857805640b42a9d3dc56e73ac4fa3dd37
Jaja M. G <77393559+jajagml@users.noreply.github.com>
2023-11-23 14:33:29 +0800

    Update FileHandling.py
    file hanlding

--- PythonProj/FileHandling.py
+++ PythonProj/FileHandling.py
@@ -1,8 +1,8 @@
 # python - file handling
 
 # open() - function responsible for opening a file for reading
-myFile = open("MyTextFile.txt", "r")
-# myFile = open("MyTextFile.txt", "rt") - read the text
+# myFile = open("MyTextFile.txt", "r")
+# myFile = open("MyTextFile.txt", "rt")  # open the file and returns file object
 # "r" - Read - Default value. Opens a file for reading, error if the file does not exist
 # "a" - Append - Opens a file for appending, creates the file if it does not exist
 # "w" - Write - Opens a file for writing, creates the file if it does not exist
@@ -10,20 +10,27 @@ myFile = open("MyTextFile.txt", "r")
 # "t" - Text - Default value. Text mode
 # "b" - Binary - Binary mode (e.g. images)
 
-# print(myFile.read(17))
-print(myFile.readline())  # not working to be investigate
-for line in myFile:
-    print(line)
+# myFile = open("MyTextFile.txt", "rt") # read text
+# print(myFile.read()) # returns the whole text
+# print(myFile.read(8)) # specify how many characters you want to return
+# print(myFile.readline())  # return one line only
+# for line in myFile:
+#     print(line)
 
-myFile.close() # good practice to close the file
+# myFile = open("MyTextFile.txt", "a") # append
+# myFile.write("\nI am the new line")
+
+# myFile = open("MyTextFile.txt", "w") # write or overwrite
+# myFile.write("\nI delete them all")
+
+# myFile = open("myfile.txt", "w") # create new file if the specified file does not exist | w-write
+# myFile = open("myfile.txt", "a") # create new file if the specified file does not exist same with Write from line 26 | a-append
+# myFile = open("myfile.txt", "x") # create new file but return error if the file already existing | x-create
+
+# myFile.close() # good practice to close the file
 
 
+import os # this library is used to delete file
 
-
-
-
-
-
-
-
-
+# os.remove("zzz.txt") # remove/delete file | remove
+# os.rmdir("myfolder") # remove/delete folder | rmdr
