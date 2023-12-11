0d193ba3a6eeb9599f9ebf8045ad1437aaacc48e
jerold-pascua <60119463+jerold-pascua@users.noreply.github.com>
2023-12-11 09:32:03 +0800

    Update Classes.py

--- Basics/Classes.py
+++ Basics/Classes.py
@@ -1,1 +1,15 @@
+class Chocolate:
 
+  def __init__(self, name, type, size, weight):
+    self.name = name
+    self.type = type
+    self.size = size
+    self._weight = weight
+
+@property
+def weight(self):
+  return self._weight
+
+@weight.setter
+def weight(self, new_weight):
+  self._weight = new_weight
