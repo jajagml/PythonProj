83e22886a050523298e6323bad12144e12aaf66d
jerold-pascua <60119463+jerold-pascua@users.noreply.github.com>
2023-12-11 09:55:20 +0800

    Update Classes.py

--- Basics/Classes.py
+++ Basics/Classes.py
@@ -1,10 +1,13 @@
 class Chocolate:
 
-  def __init__(self, name, type, size, weight):
+  PRICE_MULTIPLIER = 3.5
+  
+  def __init__(self, name, type, size, weight=100):
     self.name = name
     self.type = type
-    self.size = size
+    self._size = _size
     self._weight = weight
+    self._price = weight * price_multiplier
 
 @property
 def weight(self):
@@ -12,4 +15,37 @@ def weight(self):
 
 @weight.setter
 def weight(self, new_weight):
-  self._weight = new_weight
+  if isinstance(new_weight, float) and new_weight > 0:
+    self._weight = new_weight
+  else:
+    print("Please enter a valid weight)
+
+@property
+def price(self):
+  return self._price
+
+@property
+def size(self):
+  return self._size
+
+@size.setter
+def size(self, new_size)
+  size_list = ["S", "M", "L"]
+  if new_size in size_list:
+    self._size = new_size
+  else:
+    print("Please enter a valid size")
+
+def get_number_of_servings():
+  match self._size:
+    case "S":
+      return 1
+    case "M":
+      return 2
+    case "L":
+      return 4
+    case _:
+      return 0
+
+
+    
