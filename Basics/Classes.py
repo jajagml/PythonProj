84c59f711f1579fcd7fe58500440c51d3ed8473d
jerold-pascua <60119463+jerold-pascua@users.noreply.github.com>
2023-12-11 09:56:02 +0800

    Update Classes.py
    fixed index

--- Basics/Classes.py
+++ Basics/Classes.py
@@ -9,43 +9,43 @@ class Chocolate:
     self._weight = weight
     self._price = weight * price_multiplier
 
-@property
-def weight(self):
-  return self._weight
-
-@weight.setter
-def weight(self, new_weight):
-  if isinstance(new_weight, float) and new_weight > 0:
-    self._weight = new_weight
-  else:
-    print("Please enter a valid weight)
-
-@property
-def price(self):
-  return self._price
-
-@property
-def size(self):
-  return self._size
-
-@size.setter
-def size(self, new_size)
-  size_list = ["S", "M", "L"]
-  if new_size in size_list:
-    self._size = new_size
-  else:
-    print("Please enter a valid size")
-
-def get_number_of_servings():
-  match self._size:
-    case "S":
-      return 1
-    case "M":
-      return 2
-    case "L":
-      return 4
-    case _:
-      return 0
-
-
-    
+  @property
+  def weight(self):
+    return self._weight
+  
+  @weight.setter
+  def weight(self, new_weight):
+    if isinstance(new_weight, float) and new_weight > 0:
+      self._weight = new_weight
+    else:
+      print("Please enter a valid weight)
+  
+  @property
+  def price(self):
+    return self._price
+  
+  @property
+  def size(self):
+    return self._size
+  
+  @size.setter
+  def size(self, new_size)
+    size_list = ["S", "M", "L"]
+    if new_size in size_list:
+      self._size = new_size
+    else:
+      print("Please enter a valid size")
+  
+  def get_number_of_servings():
+    match self._size:
+      case "S":
+        return 1
+      case "M":
+        return 2
+      case "L":
+        return 4
+      case _:
+        return 0
+  
+  
+      
